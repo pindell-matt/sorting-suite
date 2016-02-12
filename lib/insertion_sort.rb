467032ddae26801e6b75a913a50836a6f132e935
Matt Pindell <pindell.m@gmail.com>
2016-02-11 18:20:15 -0700

    refactor: insertion sort

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -2,23 +2,6 @@ require 'pry'
 
 class InsertionSort
 
-  # def sort(array)
-  #   array.flatten!
-  #   a_length = array.length
-  #   (a_length - 1).times do |iter|
-  #     count = iter
-  #     while iter >= 0 && array[iter] > array[iter + 1]
-  #       smaller = array[iter + 1]
-  #       larger  = array[iter]
-  #
-  #       array[iter]     = smaller
-  #       array[iter + 1] = larger
-  #       count -= 1
-  #     end
-  #   end
-  #   array
-  # end
-
   def sort(array)
     array.flatten!
     (array.length - 1).times do |iter|
