df93bd7c8fbb8c8fd503e0de512cba90b4c4f43b
Matt Pindell <pindell.m@gmail.com>
2016-02-14 13:22:54 -0700

    updated sorting_suite to support all algorithms

--- lib/sorting_suite.rb
+++ lib/sorting_suite.rb
@@ -1,8 +1,28 @@
 require_relative 'bubble_sort'
+require_relative 'insertion_sort'
+require_relative 'merge_sort'
 require 'pry'
 require 'benchmark'
 
 class SortingSuite
 
+  def bubble_sort(array)
+    BubbleSort.new.sort(array)
+  end
 
+  def insertion_sort(array)
+    InsertionSort.new.sort(array)
+  end
+
+  def merge_sort(array)
+    MergeSort.new.sort(array)
+  end
+
+end
+
+if __FILE__ == $0
+  sorter = SortingSuite.new
+  sorter.bubble_sort([3, 2, 1])
+  sorter.insertion_sort([3, 2, 1])
+  sorter.merge_sort([3, 2, 1])
 end
