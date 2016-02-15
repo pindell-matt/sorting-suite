d514683618acfd6b0218ecb1a881d5b984baaac8
Matt Pindell <pindell.m@gmail.com>
2016-02-15 00:29:20 -0700

    method: fastest and slowest for benchmark

--- lib/sorting_suite.rb
+++ lib/sorting_suite.rb
@@ -43,8 +43,32 @@ module SortingSuite
 
   class Bench
     def time(method, array)
-      var = Benchmark.measure {method.new(array)}
-      puts "#{method.to_s.split("::")[1]} Sort took #{var.real.round(6)} seconds"
+      var = benchmark(method, array)
+      puts "#{method.to_s.split("::")[1]}Sort took #{var.real.round(6)} seconds"
+    end
+
+    def benchmark(method, array)
+      Benchmark.measure {method.new(array)}
+    end
+
+    def fastest(array)
+      bubble    = benchmark(SortingSuite::Bubble, (array)).real
+      insertion = benchmark(SortingSuite::Insertion, (array)).real
+      selection = benchmark(SortingSuite::Selection, (array)).real
+      merge     = benchmark(SortingSuite::Merge, (array)).real
+
+      all = {'bubble' => bubble, 'insertion' => insertion, 'selection' => selection, 'merge' => merge}
+      puts "#{all.key(all.values.min).capitalize}Sort is the fastest"
+    end
+
+    def slowest(array)
+      bubble    = benchmark(SortingSuite::Bubble, (array)).real
+      insertion = benchmark(SortingSuite::Insertion, (array)).real
+      selection = benchmark(SortingSuite::Selection, (array)).real
+      merge     = benchmark(SortingSuite::Merge, (array)).real
+
+      all = {'bubble' => bubble, 'insertion' => insertion, 'selection' => selection, 'merge' => merge}
+      puts "#{all.key(all.values.max).capitalize}Sort is the slowest"
     end
   end
 
@@ -52,12 +76,15 @@ end
 
 if __FILE__ == $0
   array = [5, 4, 3, 2, 1]
-  SortingSuite::Bubble.new(array)
-  SortingSuite::Insertion.new(array).inplace_sort
-  SortingSuite::Insertion.new(array).sort
-  SortingSuite::Merge.new(array)
-  SortingSuite::Selection.new(array)
+  # SortingSuite::Bubble.new(array)
+  # SortingSuite::Insertion.new(array).inplace_sort
+  # SortingSuite::Insertion.new(array).sort
+  # SortingSuite::Merge.new(array)
+  # SortingSuite::Selection.new(array)
 
   benchmark = SortingSuite::Bench.new
   benchmark.time(SortingSuite::Bubble, (array))
+  benchmark.time(SortingSuite::Merge, (array))
+  benchmark.fastest(array)
+  benchmark.slowest(array)
 end
